3ba1fcc6fd5a98cee2d0aad4022cb2469ccf3cb7
kentlan <salsicom@gmail.com>
2018-07-28 15:52:35 +0300

    first commit

---
+++ config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = 'AIzaSyDW_63dDW7ArHOVPSb5Kd6BMPUaQ1OJHjM'
+export const FIREBASE_AUTH_DOMAIN = 'circle-of-friends-26cca.firebaseapp.com'
+export const FIREBASE_DATABASE_URL = 'https://circle-of-friends-26cca.firebaseio.com'
+export const FIREBASE_PROJECT_ID = 'circle-of-friends-26cca'
+export const FIREBASE_STORAGE_BUCKET = 'circle-of-friends-26cca.appspot.com'
+export const FIREBASE_MESSAGING_SENDER_ID = '971597508083'
+
+export const FACEBOOK_APP_ID = '1094496300703576'
